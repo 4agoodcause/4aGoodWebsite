a3014066d392b7e9b66ac1f0ebd86925bf8def2c
Brad Sawicki <git@bradsawicki.com>
2015-05-06 12:41:23 -0500

    Features page / imgs

---
+++ features.php
@@ -0,0 +1,119 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/header.php'; ?>
+
+  <section class="hero hero-blue">
+    <div class="wrapper">
+      <h1>Unlimited, mobile-friendly fundraising</h1>
+      <p>No annual contracts, no set up fees, no monthly minimums, no early termination fees.</p>
+      <p><a href="" class="btn btn-blue">Request a Demo</a></p>
+    </div>
+  </section>
+
+  <section class="section-feature-01">
+    <div class="wrapper">
+      <div class="row">
+        <div class="col-1-2 col-content col-content-left">
+          <h2 class="section-title">Convert more gifts with mobile responsive pages</h2>
+          <p>When your donors go online, 60% of the time they are on a mobile device. These donors want a simple, easy giving experience. Boost your online donation conversions and your average gift size by greeting these donors, on any device, with our simple, mobile-friendly pages.</p>
+        </div>
+        <div class="col-1-2 col-devices col-devices-right">
+          <img src="assets/images/devices-02.jpg" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-feature-02">
+    <div class="wrapper">
+      <h2 class="section-title-center">Payment pages with no limits</h2>
+      <div class="row">
+        <div class="col-1-2 col-devices col-devices-left">
+          <img src="assets/images/browser-shots-01.jpg" alt="">
+        </div>
+        <div class="col-1-2 col-content col-content-right">
+          <h3>As many payment pages as you need</h3>
+          <p>Receive unlimited payment pages for one flat monthly fee. We'll do all the work to set up your first page and there are no fees when you need additional payment pages or changes to your existing pages.</p>
+          <h3>Take payment for almost any purpose</h3>
+          <p>Receive access to more payment page options than any other provider. Accept payments for online donations, event registration, membership, program fees, tuition, products sales, and more.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-feature-03">
+    <div class="wrapper">
+      <h2 class="section-title-center">Track your results with easy reporting and alerts</h2>
+      <div class="row">
+        <div class="col-1-2 col-content col-content-left">
+          <h3>Export reports to Excel and CSV</h3>
+          <p>Track your success with our reporting center where you can view or download all your donor information as well as remotely manage aspects of your payment pages.</p>
+          <h3>Real-time email alerts</h3>
+          <p>Never miss a donation or membership payment with our automated email alerts that deliver all of the transaction details right to your inbox.</p>
+        </div>
+        <div class="col-1-2 col-devices col-devices-right">
+          <img src="assets/images/graphs-01.png" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-feature-04">
+    <div class="wrapper">
+      <div class="row">
+        <div class="col-1-2 col-devices col-devices-left">
+          <img src="assets/images/devices-03.jpg" alt="">
+        </div>
+        <div class="col-1-2 col-content col-content-right">
+          <h2 class="section-title">Expand your fundraising with peer-to-peer giving</h2>
+          <p>Empower your constituents to fundraise on your behalf or build your own crowd-raising fundraising pages with your very-own peer-to-peer giving microsite. Built to match your brand, this destination website becomes the fundraising platform for all your supporters.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-features-list">
+    <div class="wrapper">
+      <div class="row">
+        <div class="col-1-2">
+          <h3>Social sharing</h3>
+          <p>Multiply the impact of a gift with easy social media sharing tools that allow donors to tell friends and family, on Facebook and other sites, why they are supporting you.</p>
+        </div>
+        <div class="col-1-2">
+          <h3>Monthly giving</h3>
+          <p>Sustain your organization with automated, recurring donations. Our monthly giving pages and administration tools make it easy.</p>
+        </div>
+        <div class="col-1-2">
+          <h3>Payment tools</h3>
+          <p>Accept payments anywhere - online, over the phone and in-person - with our payment pages, virtual terminal, a mobile app and other payment tools.</p>
+        </div>
+        <div class="col-1-2">
+          <h3>Safe and secure</h3>
+          <p>With our SSL security, certified PCI compliance and weekly vulnerability scanning audits your donors are kept safe and secure.</p>
+        </div>
+        <div class="col-1-2">
+          <h3>Social sharing</h3>
+          <p>Multiply the impact of a gift with easy social media sharing tools that allow donors to tell friends and family, on Facebook and other sites, why they are supporting you.</p>
+        </div>
+        <div class="col-1-2">
+          <h3>Monthly giving</h3>
+          <p>Sustain your organization with automated, recurring donations. Our monthly giving pages and administration tools make it easy.</p>
+        </div>
+        <div class="col-1-2">
+          <h3>Payment tools</h3>
+          <p>Accept payments anywhere - online, over the phone and in-person - with our payment pages, virtual terminal, a mobile app and other payment tools.</p>
+        </div>
+        <div class="col-1-2">
+          <h3>Safe and secure</h3>
+          <p>With our SSL security, certified PCI compliance and weekly vulnerability scanning audits your donors are kept safe and secure.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+    
+  <section class="bottom-cta">
+    <div class="wrapper">
+      <h2>Have questions? Get in touch. We'd love to chat.</h2>
+      <p><a href="" class="btn">Request a Demo</a> <a href="" class="btn btn-outline">Contact Us</a></p>
+    </div>
+  </section>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'; ?>
