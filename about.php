71733b954697e4360536aefa678b14d325a2b087
Brad Sawicki <git@bradsawicki.com>
2015-05-06 20:26:14 -0500

    About Partners section

---
+++ about.php
@@ -0,0 +1,123 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/header.php'; ?>
+
+  <section class="hero hero-blue hero-about">
+    <div class="wrapper">
+      <h1>We're a small group of consultants, developers and designers with one simple mission:</h1>
+      <p>Help your nonprofit raise more money online</p>
+    </div>
+  </section>
+
+  <section class="section-our-promise">
+    <div class="wrapper">
+      <h2 class="section-title">Our promise to you</h2>
+      <ul>
+        <li>We'll make it easy for donors to give you money online.</li>
+        <li>You'll pay low, fair fees that keep more of what you raise in your pocket.</li>
+        <li>You'll get fast, personal customer service from a real person whenever you need it.</li>
+      </ul>
+    </div>
+  </section>
+
+  <section class="section-founder">
+    <div class="wrapper">
+      <div class="col col-founder-image">
+        <img src="assets/images/pic-ronald.jpg" alt="Ronald Pruitt">
+      </div>
+      <div class="col col-founder-bio">
+        <h3>Ronald Pruitt <span class="founder-title">Founder of 4aGoodCause</span></h3>
+        <p>Ronald founded 4aGoodCause in 1998 because of a love for building websites and a passion to make a difference in the world. He's helped build hundreds of websites that have raised millions of dollars for nonprofits from Maine to Hawaii. This extensive experience informs 4aGoodCause on how to build websites that generate big returns for the good causes we work for. Ronald is passionate about his wife and kids, his client's causes, all things Marvel, movie poster art, Auburn football, Atlanta Hawks basketball and his extensive music collection. If you call he'll promise to pause that Eric Clapton song and answer any question you have.</p>
+        <p><a href="" class="founder-linkedin">Connect on LinkedIn <svg class="icon-arrow"><use xlink:href="#icon-arrow" /></svg></a></p>
+      </div>
+    </div>
+  </section>
+
+  <section class="about-testimonials section-testimonals">
+    <div class="wrapper">
+      <div class="row">
+        <div class="col-1-2">
+          <h2 class="section-title">We work with some great causes</h2>
+          <p class="testimonial-quote"><span class="quote-mark">&ldquo;</span>4aGoodCause has provided us with responsive, personalized service, always willing to go the extra mile to help with our specific needs.</p>
+          <div class="testimonial-info">
+            <img src="assets/images/headshot-deborah-hakes.jpg" alt="Deborah Hakes" class="testimonial-headshot">
+            <p class="testimonial-name-org"><span class="testimonial-name">Deborah Hakes</span> of United Methodist Children's Home</p>
+          </div>
+        </div>
+        <div class="col-1-2 col-testimonial-logos">
+          <ul class="logo-grid">
+            <li><img src="assets/images/logo-girl-scouts.png" alt=""></li>
+            <li><img src="assets/images/logo-wellstar.png" alt=""></li>
+            <li><img src="assets/images/logo-uswf.png" alt=""></li>
+            <li><img src="assets/images/logo-concordia.png" alt=""></li>
+            <li><img src="assets/images/logo-afa.png" alt=""></li>
+            <li><img src="assets/images/logo-dallas-stars.png" alt=""></li>
+         </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-partners">
+    <div class="wrapper">
+      <h2 class="section-title-center">Our Partners</h2>
+      <div class="featured-partner">
+        <div class="partner-logo">
+          <img src="assets/images/logo-partner-dharma.png" alt="Dharma Merchant Services">
+        </div>
+        <h3>Dharma Merchant Services</h3>
+        <p>Dharma Merchant Services provides credit card processing to our clients. Dharma Merchant Services is the #1 rated merchant services provider for the nonprofit community. We share their belief in commerce with compassion and are proud to partner with a company with such a strong commitment to nonprofits.</p>
+      </div>
+      <div class="row partner-list">
+        <div class="col-1-3">
+          <div class="partner-logo">
+            <img src="assets/images/logo-partner-alertsite.png" alt="Alertsite">
+          </div>
+          <h3>AlertSite</h3>
+          <p>AlertSite is a security and performance monitoring service that keeps our clients' data safe and our sites available at all times.</p>
+        </div>
+        <div class="col-1-3">
+          <div class="partner-logo">
+            <img src="assets/images/logo-partner-authorize.png" alt="Authorize.net">
+          </div>
+          <h3>Authorize.net</h3>
+          <p>A leading payment gateway, Authorize.Net manages the complex routing of transaction data from your website to the credit card processing networks.</p>
+        </div>
+        <div class="col-1-3">
+          <div class="partner-logo">
+            <img src="assets/images/logo-partner-thawte.png" alt="Thawte">
+          </div>
+          <h3>Thawte</h3>
+          <p>All e-commerce sites hosted with 4aGoodCause are protected with SSL certificates provided by Thawte.</p>
+        </div>
+        <div class="col-1-3">
+          <div class="partner-logo">
+            <img src="assets/images/logo-partner-givingtuesday.png" alt="Giving Tuesday">
+          </div>
+          <h3>Giving Tuesday</h3>
+          <p>4aGoodCause is a founding partner of #GivingTuesday, a national day for giving back.</p>
+        </div>
+        <div class="col-1-3">
+          <div class="partner-logo">
+            <img src="assets/images/logo-partner-twelvex.png" alt="TwelveX">
+          </div>
+          <h3>TwelveX</h3>
+          <p>TwelveX provides consulting dedicated to designing, launching and growing monthly giving programs.</p>
+        </div>
+        <div class="col-1-3">
+          <div class="partner-logo">
+            <img src="assets/images/logo-partner-double.png" alt="Double the Donation">
+          </div>
+          <h3>Double the Donation</h3>
+          <p>Double the Donation provides 4aGodCause clients with the tools they need to make their matching gift programs successful.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <section class="bottom-cta">
+    <div class="wrapper">
+      <h2>Have questions? Get in touch. We'd love to chat.</h2>
+      <p><a href="" class="btn">Request a Demo</a> <a href="" class="btn btn-outline">Contact Us</a></p>
+    </div>
+  </section>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'; ?>
