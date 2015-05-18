68b97437f529e30f88adac8e253795d4d7d32b31
Brad Sawicki <git@bradsawicki.com>
2015-05-18 15:12:56 -0500

    Sidebar Icons

--- blog.php
+++ blog.php
@@ -57,7 +57,7 @@
         </div>
 
         <div class="block block-tips">
-          <h3 class="block-title">Fundraising Tips</h3>
+          <h3 class="block-title"><svg class="icon-tips"><use xlink:href="#icon-tips" /></svg> Fundraising Tips</h3>
           <p>Free online fundraising tips delivered directly to your inbox (once a week).</p>
           <form class="newsletter-form">
             <div class="form-input-wrap">
@@ -69,63 +69,8 @@
         </div>
       
       </section>
-      
-      <aside class="sidebar sidebar-right">
-      
-        <div class="block block-search">
-          <form class="search-form">
-            <div class="form-input-wrap">
-              <label for="">Search Articles</label>
-              <input type="text">
-            </div>
-            <button class="btn-search"><svg class="icon-search"><use xlink:href="#icon-search" /></svg></button>
-          </form>
-        </div>
 
-        <div class="block block-tips">
-          <h3 class="block-title">Fundraising Tips</h3>
-          <p>Free online fundraising tips delivered directly to your inbox (once a week).</p>
-          <form class="newsletter-form">
-            <div class="form-input-wrap">
-              <label for="">Email Address</label>
-              <input type="email">
-            </div>
-            <button class="btn-submit">Subscribe</button>
-          </form>
-        </div>
-        
-        <div class="block block-social-links">
-          <h3 class="block-title">Follow Us</h3>
-          <ul class="sidebar-social">
-            <li><a href=""><svg class="icon-circle-facebook"><use xlink:href="#icon-circle-facebook" /></svg></a></li>
-            <li><a href=""><svg class="icon-circle-twitter"><use xlink:href="#icon-circle-twitter" /></svg></a></li>
-            <li><a href=""><svg class="icon-circle-linkedin"><use xlink:href="#icon-circle-linkedin" /></svg></a></li>
-          </ul>
-        </div>
-        
-        <div class="block block-link-list block-categories">
-          <h3 class="block-title">Categories</h3>
-          <ul>
-            <li><a href="">Company and Client News</a></li>
-            <li><a href="">Ecommerce and Online Giving</a></li>
-            <li><a href="">Mobile</a></li>
-            <li><a href="">Nonprofit Marketing</a></li>
-            <li><a href="">Web Design</a></li>
-          </ul>
-        </div>
-
-        <div class="block block-link-list block-popular-posts">
-          <h3 class="block-title">Categories</h3>
-          <ul>
-            <li><a href="">What the NBA can teach us about fundraising</a></li>
-            <li><a href="">$11.6 million raised online by 4aGoodCause nonprofits in 2014</a></li>
-            <li><a href="">10 places to put a link to your online donation page</a></li>
-            <li><a href="">Make your website more shareable by using social media tags</a></li>
-            <li><a href="">10 nonprofit marketing & technology blogs you should follow</a></li>
-          </ul>
-        </div>
-
-      </aside>
+      <?php include $_SERVER['DOCUMENT_ROOT'].'/views/sidebar.php'; ?>      
 
     </div>
   </div>
